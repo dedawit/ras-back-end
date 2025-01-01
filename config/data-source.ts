52f5028ad4a5ef5f9cbb0915b491904309202121
dedawit <realdavis7779@gmail.com>
2025-01-01 19:04:49 +0300

    rfq module in progress

--- config/data-source.ts
+++ config/data-source.ts
@@ -5,12 +5,12 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: process.env.DATABASE_TYPE as 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
+  host: process.env.HOST_NAME,
+  port: +process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  synchronize: true, 
+  synchronize: true,
   logging: false,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/src/migrations/*{.ts,.js}'],
