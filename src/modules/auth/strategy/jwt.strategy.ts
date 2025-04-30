d06f0508fe4ed1a73b6daaf15f120b336ea055f7
dedawit <realdavis7779@gmail.com>
2025-04-30 20:20:33 +0300

    logout added

--- src/modules/auth/strategy/jwt.strategy.ts
+++ src/modules/auth/strategy/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserService } from 'src/modules/user/usecase/user.service';
+import { JwtPayload } from '../interface/jwt-payload.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
@@ -13,7 +14,13 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload: any) {
-    return this.userService.findById(payload.sub);
+  async validate(payload: JwtPayload) {
+    // Whatever you return here gets attached to `req.user`
+    return {
+      sub: payload.sub, // User ID
+      email: payload.email,
+      lastRole: payload.lastRole,
+      tokenVersion: payload.tokenVersion,
+    };
   }
 }
